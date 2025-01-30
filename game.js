83b151606ae61d16aa0b373e9c133764f376626f
UonomeUota <okuwing@yahoo.co.jp>
2025-01-30 21:58:04 +0900

    歩く画像を追加、ゲームシーンに遷移するコードを追加

--- game.js
+++ game.js
@@ -1,6 +1,7 @@
 // src/game.js
 
 class StartScene extends Phaser.Scene {
+    //スタート画面を作成
     constructor() {
         super({ key: 'StartScene' });
         this.textScaleDirection = 1; // テキストのスケールの方向（1: 拡大, -1: 縮小）
@@ -51,24 +52,37 @@ class StartScene extends Phaser.Scene {
 class GameScene extends Phaser.Scene {
     constructor() {
         super({ key: 'GameScene' });
+        this.candies = []; // お菓子の配列
+        this.enemies = []; // 敵の配列
+        this.score = 0; // スコア
     }
 
-    preload(){
+    preload() {
         this.load.image('player', 'png/player.png');
         this.load.image('candy', 'png/candy.png');
     }
 
     create() {
-        this.add.image(config.width / 2, config.height / 2, 'startImage');
+        // 画面の中央にプレイヤーキャラクターを配置
+        this.player = this.physics.add.sprite(config.width / 2, config.height / 2, 'player').setOrigin(0.5);
+        this.player.setCollideWorldBounds(true); // プレイヤーが画面の端に衝突しないようにする
 
-        this.player = this.physics.add.sprite(config.width / 2, config.height - 30, 'player').setOrigin(0.5);
-        this.player.setCollideWorldBounds(true);
-
-        this.cursors = this.input.keyboard.createCursorKeys();
+        this.cursors = this.input.keyboard.createCursorKeys(); // キーボードの入力を取得
 
         this.candyTimer = this.time.addEvent({
             delay: 1000,
             callback: this.throwCandy,
+            callbackScope: this,
+            loop: true
+        });
+
+        // スコア表示用のテキスト
+        this.scoreText = this.add.text(16, 16, 'Score: 0', { fontSize: '16px', fill: '#fff' });
+
+        // 敵を生成するタイマー
+        this.enemyTimer = this.time.addEvent({
+            delay: 2000,
+            callback: this.spawnEnemy,
             callbackScope: this,
             loop: true
         });
@@ -84,19 +98,43 @@ class GameScene extends Phaser.Scene {
             this.player.setVelocityX(0);
         }
 
-        // お菓子の衝突処理
-        this.physics.overlap(this.player, candies, this.hitCandy, null, this);
+        if (this.cursors.up.isDown) {
+            this.player.setVelocityY(-160);
+        } else if (this.cursors.down.isDown) {
+            this.player.setVelocityY(160);
+        } else {
+            this.player.setVelocityY(0);
+        }
+
+        // 敵の移動処理
+        this.enemies.forEach(enemy => {
+            this.physics.moveToObject(enemy, this.player, 100);
+        });
+
+        // プレイヤーと敵の衝突処理
+        this.physics.overlap(this.player, this.enemies, this.gameOver, null, this);
     }
 
     throwCandy() {
         const candy = this.physics.add.sprite(Phaser.Math.Between(0, config.width), 0, 'candy');
         candy.setVelocity(0, 200);
-        candies.push(candy);
+        this.candies.push(candy);
+    }
+
+    spawnEnemy() {
+        const enemy = this.physics.add.sprite(Phaser.Math.Between(0, config.width), 0, 'enemy');
+        enemy.setVelocity(0, 100);
+        this.enemies.push(enemy);
+    }
+
+    gameOver() {
+        this.scene.restart(); // ゲームオーバー時にシーンを再起動
     }
 
     hitCandy(player, candy) {
         candy.destroy(); // お菓子を削除
-        // ゲームオーバー処理をここに追加することができます
+        this.score += 10; // スコアを増加
+        this.scoreText.setText('Score: ' + this.score); // スコアを更新
     }
 }
 
